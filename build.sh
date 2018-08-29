1aaeaa605322c641efa357002963b92fcd919f66
Peter Murphy <pmurphy@es.net>
2018-08-29 10:36:08 -0600

    Add pond proto file

--- build.sh
+++ build.sh
@@ -4,7 +4,9 @@
 # Script to compile protobuf files
 #
 
-PROTO_PATH="netbeam"
+NETBEAM_PROTO_PATH="netbeam"
+POND_PROTO_PATH="pond"
+
 PROTO_GEN_TS_PATH="./node_modules/.bin/protoc-gen-ts"
 GRPC_NODE_PLUGIN_PATH="./node_modules/grpc-tools/bin/grpc_node_plugin"
 
@@ -12,10 +14,14 @@ GRPC_NODE_PLUGIN_PATH="./node_modules/grpc-tools/bin/grpc_node_plugin"
 # protoc -I=$PROTO_PATH --include_imports --include_source_info netbeam.proto --descriptor_set_out v1/netbeam.pb
 # protoc -I=$PROTO_PATH --include_imports --include_source_info network.proto --descriptor_set_out v1/network.pb
 
-# Javascript
-./node_modules/.bin/grpc_tools_node_protoc --js_out=import_style=commonjs,binary:./js --grpc_out=./js --plugin=protoc-gen-grpc=$GRPC_NODE_PLUGIN_PATH --proto_path=$PROTO_PATH netbeam.proto google/api/http.proto google/api/annotations.proto google/protobuf/compiler/plugin.proto 
-./node_modules/.bin/grpc_tools_node_protoc --js_out=import_style=commonjs,binary:./js --grpc_out=./js --plugin=protoc-gen-grpc=$GRPC_NODE_PLUGIN_PATH --proto_path=$PROTO_PATH network.proto
+# Javascript netbeam build
+./node_modules/.bin/grpc_tools_node_protoc --js_out=import_style=commonjs,binary:./js --grpc_out=./js --plugin=protoc-gen-grpc=$GRPC_NODE_PLUGIN_PATH --proto_path=$NETBEAM_PROTO_PATH netbeam.proto google/api/http.proto google/api/annotations.proto google/protobuf/compiler/plugin.proto 
+./node_modules/.bin/grpc_tools_node_protoc --js_out=import_style=commonjs,binary:./js --grpc_out=./js --plugin=protoc-gen-grpc=$GRPC_NODE_PLUGIN_PATH --proto_path=$NETBEAM_PROTO_PATH network.proto
 
 #Typescript
 #protoc --js_out=import_style=commonjs,binary:$OUT_DIR --ts_out=service=true:$OUT_DIR --plugin=protoc-gen-ts=$PROTO_GEN_TS_PATH --proto_path=$PROTO_PATH netbeam.proto
 #protoc --js_out=import_style=commonjs,binary:$OUT_DIR --ts_out=service=true:$OUT_DIR --plugin=protoc-gen-ts=$PROTO_GEN_TS_PATH --proto_path=$PROTO_PATH network.proto
+
+# Javascript pond build
+./node_modules/.bin/grpc_tools_node_protoc --js_out=import_style=commonjs,binary:./js --grpc_out=./js --plugin=protoc-gen-grpc=$GRPC_NODE_PLUGIN_PATH --proto_path=$POND_PROTO_PATH pond.proto
+./node_modules/.bin/grpc_tools_node_protoc -I pond --go_out=go --proto_path=$POND_PROTO_PATH pond.proto
--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const netbeam_pb = require("./js/netbeam_pb");
 const netbeam_grpc = require("./js/netbeam_grpc_pb");
 const network_pb = require("./js/network_pb");
 const network_grpc = require("./js/network_grpc_pb");
+const pond_pb = require("./js/pond_pb");
 
 module.exports = {
+    pond: pond_pb,
     netbeamCore: netbeam_pb,
     netbeamCoreServices: netbeam_grpc,
     netbeamNetwork: network_pb,
