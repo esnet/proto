2bac962d4ff52204ba128867ec2141e5dcbe34f5
Peter Murphy <pmurphy@es.net>
2019-09-23 06:24:00 -0700

    Add back the bytes type, remove the SayHello rpc

--- js/netbeam_grpc_pb.d.ts
+++ js/netbeam_grpc_pb.d.ts
@@ -8,7 +8,6 @@ import * as netbeam_pb from "./netbeam_pb";
 import * as pond_pb from "./pond_pb";
 
 interface INetbeamService extends grpc.ServiceDefinition<grpc.UntypedServiceImplementation> {
-    sayHello: INetbeamService_ISayHello;
     fetchTile: INetbeamService_IFetchTile;
     listOrganizations: INetbeamService_IListOrganizations;
     getOrganization: INetbeamService_IGetOrganization;
@@ -24,15 +23,6 @@ interface INetbeamService extends grpc.ServiceDefinition<grpc.UntypedServiceImpl
     createRule: INetbeamService_ICreateRule;
 }
 
-interface INetbeamService_ISayHello extends grpc.MethodDefinition<netbeam_pb.HelloRequest, netbeam_pb.HelloReply> {
-    path: string; // "/netbeam.Netbeam/SayHello"
-    requestStream: boolean; // false
-    responseStream: boolean; // false
-    requestSerialize: grpc.serialize<netbeam_pb.HelloRequest>;
-    requestDeserialize: grpc.deserialize<netbeam_pb.HelloRequest>;
-    responseSerialize: grpc.serialize<netbeam_pb.HelloReply>;
-    responseDeserialize: grpc.deserialize<netbeam_pb.HelloReply>;
-}
 interface INetbeamService_IFetchTile extends grpc.MethodDefinition<netbeam_pb.TileRequest, pond_pb.TimeSeries> {
     path: string; // "/netbeam.Netbeam/FetchTile"
     requestStream: boolean; // false
@@ -154,7 +144,6 @@ interface INetbeamService_ICreateRule extends grpc.MethodDefinition<netbeam_pb.C
 export const NetbeamService: INetbeamService;
 
 export interface INetbeamServer {
-    sayHello: grpc.handleUnaryCall<netbeam_pb.HelloRequest, netbeam_pb.HelloReply>;
     fetchTile: grpc.handleUnaryCall<netbeam_pb.TileRequest, pond_pb.TimeSeries>;
     listOrganizations: grpc.handleUnaryCall<netbeam_pb.OrganizationListRequest, netbeam_pb.OrganizationList>;
     getOrganization: grpc.handleUnaryCall<netbeam_pb.OrganizationRequest, netbeam_pb.Organization>;
@@ -171,9 +160,6 @@ export interface INetbeamServer {
 }
 
 export interface INetbeamClient {
-    sayHello(request: netbeam_pb.HelloRequest, callback: (error: grpc.ServiceError | null, response: netbeam_pb.HelloReply) => void): grpc.ClientUnaryCall;
-    sayHello(request: netbeam_pb.HelloRequest, metadata: grpc.Metadata, callback: (error: grpc.ServiceError | null, response: netbeam_pb.HelloReply) => void): grpc.ClientUnaryCall;
-    sayHello(request: netbeam_pb.HelloRequest, metadata: grpc.Metadata, options: Partial<grpc.CallOptions>, callback: (error: grpc.ServiceError | null, response: netbeam_pb.HelloReply) => void): grpc.ClientUnaryCall;
     fetchTile(request: netbeam_pb.TileRequest, callback: (error: grpc.ServiceError | null, response: pond_pb.TimeSeries) => void): grpc.ClientUnaryCall;
     fetchTile(request: netbeam_pb.TileRequest, metadata: grpc.Metadata, callback: (error: grpc.ServiceError | null, response: pond_pb.TimeSeries) => void): grpc.ClientUnaryCall;
     fetchTile(request: netbeam_pb.TileRequest, metadata: grpc.Metadata, options: Partial<grpc.CallOptions>, callback: (error: grpc.ServiceError | null, response: pond_pb.TimeSeries) => void): grpc.ClientUnaryCall;
@@ -217,9 +203,6 @@ export interface INetbeamClient {
 
 export class NetbeamClient extends grpc.Client implements INetbeamClient {
     constructor(address: string, credentials: grpc.ChannelCredentials, options?: object);
-    public sayHello(request: netbeam_pb.HelloRequest, callback: (error: grpc.ServiceError | null, response: netbeam_pb.HelloReply) => void): grpc.ClientUnaryCall;
-    public sayHello(request: netbeam_pb.HelloRequest, metadata: grpc.Metadata, callback: (error: grpc.ServiceError | null, response: netbeam_pb.HelloReply) => void): grpc.ClientUnaryCall;
-    public sayHello(request: netbeam_pb.HelloRequest, metadata: grpc.Metadata, options: Partial<grpc.CallOptions>, callback: (error: grpc.ServiceError | null, response: netbeam_pb.HelloReply) => void): grpc.ClientUnaryCall;
     public fetchTile(request: netbeam_pb.TileRequest, callback: (error: grpc.ServiceError | null, response: pond_pb.TimeSeries) => void): grpc.ClientUnaryCall;
     public fetchTile(request: netbeam_pb.TileRequest, metadata: grpc.Metadata, callback: (error: grpc.ServiceError | null, response: pond_pb.TimeSeries) => void): grpc.ClientUnaryCall;
     public fetchTile(request: netbeam_pb.TileRequest, metadata: grpc.Metadata, options: Partial<grpc.CallOptions>, callback: (error: grpc.ServiceError | null, response: pond_pb.TimeSeries) => void): grpc.ClientUnaryCall;
--- js/netbeam_pb.d.ts
+++ js/netbeam_pb.d.ts
@@ -323,55 +323,6 @@ export namespace RuleList {
     }
 }
 
-export class HelloRequest extends jspb.Message { 
-    getName(): string;
-    setName(value: string): void;
-
-
-    serializeBinary(): Uint8Array;
-    toObject(includeInstance?: boolean): HelloRequest.AsObject;
-    static toObject(includeInstance: boolean, msg: HelloRequest): HelloRequest.AsObject;
-    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
-    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
-    static serializeBinaryToWriter(message: HelloRequest, writer: jspb.BinaryWriter): void;
-    static deserializeBinary(bytes: Uint8Array): HelloRequest;
-    static deserializeBinaryFromReader(message: HelloRequest, reader: jspb.BinaryReader): HelloRequest;
-}
-
-export namespace HelloRequest {
-    export type AsObject = {
-        name: string,
-    }
-}
-
-export class HelloReply extends jspb.Message { 
-    getMessage(): string;
-    setMessage(value: string): void;
-
-
-    hasTstamp(): boolean;
-    clearTstamp(): void;
-    getTstamp(): pond_pb.Timestamp | undefined;
-    setTstamp(value?: pond_pb.Timestamp): void;
-
-
-    serializeBinary(): Uint8Array;
-    toObject(includeInstance?: boolean): HelloReply.AsObject;
-    static toObject(includeInstance: boolean, msg: HelloReply): HelloReply.AsObject;
-    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
-    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
-    static serializeBinaryToWriter(message: HelloReply, writer: jspb.BinaryWriter): void;
-    static deserializeBinary(bytes: Uint8Array): HelloReply;
-    static deserializeBinaryFromReader(message: HelloReply, reader: jspb.BinaryReader): HelloReply;
-}
-
-export namespace HelloReply {
-    export type AsObject = {
-        message: string,
-        tstamp?: pond_pb.Timestamp.AsObject,
-    }
-}
-
 export class TileRequest extends jspb.Message { 
 
     hasNamespace(): boolean;
@@ -743,9 +694,10 @@ export namespace CreateRuleRequest {
     }
 
     export enum Type {
-    COUNTER = 0,
-    GAUGE = 1,
-    STRING = 2,
+    BYTES = 0,
+    COUNTER = 1,
+    GAUGE = 2,
+    STRING = 3,
     }
 
 }
